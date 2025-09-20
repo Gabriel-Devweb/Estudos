993b84f745d7763b291dab96c4ae903ad6ca02c1
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-20 15:28:57 -0300

    Organização das pastas

---
+++ Java/Tipos primitivos/ex001/main.java
@@ -0,0 +1,7 @@
+public class main {
+  public static void main(String[] args) {
+    //int, double, flout,char, byte, short, lang, boolean
+    int idade = 10;
+    System.out.println("A idade é "+idade+" anos");
+  }
+}
--- Java/ex002/main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-  public static void main(Strings[] args) {
-    //int, double, flout,char, byte, short, lang, boolean
-    int age = 10;
-  }
-}
