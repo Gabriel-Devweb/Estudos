4fd65931422080ea20353c4388c642803f94c39f
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-20 03:18:10 -0300

    Criação da pasta do java, atualização do Readme.md do phpne criação do do java e priemiro script  java

---
+++ PHP/ex007/Index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Strings em PHP</title>
+</head>
+<body>
+  <?php 
+  //Fazendo testes
+/*$nome = "Gabriel";
+  echo "Olá $nome! <br>"; 
+  echo 'Olá $nome!';
+  $nome = "Gabriel";
+  $sobrenome = "Barreto";
+  echo "$nome $sobrenome \u{1F596}";
+  const ESTADO = "BA";
+  echo "Moro na " .ESTADO;
+  const CANAL = "Curso em vídeo \u{1F499}";
+  echo "Eu adoro o " .CANAL;
+  echo "Estamos no ano de " .date('Y');
+  $nome = "Charles";
+  $snom = "Oliveira";
+  echo "$nome \"Do Bronx\" $snom";*/
+  $nome = "Gaberiel";
+  $sobrenome = "Barreto";
+  $apelido = "Xyro";
+  echo "$nome \"$apelido\" $sobrenome <br>";
+  ?>
+</body>
+</html>
+<?php
+echo "Teste";
+?>
---
+++ PHP/teste/Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Interação com Formulário</title>
+</head>
+<body>
+  <header>
+    <h1>Apresente-se para nós</h1>
+  </header>
+  <section>
+    <form action="card.php" method="get">
+      <label for="nome">Nome</label>
+      <input type="text" name="nome" id="idnome">
+      <label for="sobrenome">sobrenome</label>
+      <input type="text" name="sobrenome" id="idsobrenome">
+      <input type="submit" value="Enviar">
+<?php
+    var_dump($_GET);
+    ?>
+    </form>
+  </section>
+</body>
+</html>
