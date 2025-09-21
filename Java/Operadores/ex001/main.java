f3d1124dcf471b40628731da7acd10acb3d6acfa
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-21 01:31:29 -0300

    Aprendendo operadores relacionais

--- Java/Operadores/ex001/main.java
+++ Java/Operadores/ex001/main.java
@@ -1,8 +1,25 @@
 public class main {
   public static void main(String[] args) {
     // + - / *
-    int numero1 = 10;
-    int numero2 = 20;
-    System.out.println(numero2+numero1+"O valor é "+numero2+numero1);
+    int numero01 = 10;
+    int numero02 = 20;
+    double resultado = numero01 / (double) numero02;
+    System.out.println(resultado);
+
+    // % 
+    int resto = 20 % 2;
+    System.out.println(resto);
+
+    // < > <= >= === !=
+    boolean isDezMaiorQueVinte = 10 > 20;
+    boolean isDezMenorQueVinte = 10 < 20;
+    boolean isDezIgualAVinte = 10 == 20;
+    boolean isDezIgualADez = 10 == 10;
+    boolean isDezdiferenteDez = 10 != 10;
+    System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+    System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+    System.out.println("isDezIgualAVinte " +isDezIgualAVinte);
+    System.out.println("isDezIgualADez " +isDezIgualADez);
+    System.out.println("isDezdiferenteDez " +isDezdiferenteDez);
   }
-}
+  }
