6ba94f746bd0b49a26a9395d37df46448250fc6d
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-21 00:49:55 -0300

    Aprendendo operadores em java

---
+++ Java/Operadores/ex001/main.java
@@ -0,0 +1,8 @@
+public class main {
+  public static void main(String[] args) {
+    // + - / *
+    int numero1 = 10;
+    int numero2 = 20;
+    System.out.println(numero2+numero1+"O valor é "+numero2+numero1);
+  }
+}
