b5905e68c2ad034f7a3b0a76901adae51e9037cc
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-21 00:35:47 -0300

    Utilizando boas praticas

--- Java/Tipos primitivos/ex002/main.java
+++ Java/Tipos primitivos/ex002/main.java
@@ -4,6 +4,7 @@ public class main {
     String endereco = "Rua jacinto pinto";
     double salario = 1518.55;
     String data = "21/09/25";
-    System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data);
+    String relatorio = "Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data;
+    System.out.println(relatorio);
   }
 }
