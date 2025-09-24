d22186a250abcb0e03c24a85ba88d30a2b29f615
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-09-23 22:20:00 -0300

    Fazendo o exercicio de outra maneira

--- Condicionais/ex003/main.java
+++ Condicionais/ex003/main.java
@@ -2,14 +2,11 @@ public class main {
   public static void main(String[] args) {
     //Doar se salário for > 5000
    double salario = 6000;
-   String mensagemDoar = "Eu vou doar 500 para o DevDojo";
+  /* String mensagemDoar = "Eu vou doar 500 para o DevDojo";
    String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
-   String resultado;
-   if (salario > 5000) {
-     resultado = mensagemDoar;
-   } else {
-    resultado = mensagemNaoDoar;
-   }
+   */
+   // (condição) ? verdadeiro : falso
+   String resultado = (salario > 500) ? "Eu vou dar 500 para o DevDojo" : "Ainda não tenho condições, mss vou ter";
    System.out.println(resultado);
   }
 }
