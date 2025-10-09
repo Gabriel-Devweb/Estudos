3cd28fb83b6474765d7d256e5acd0b2709281061
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-10-08 21:04:13 -0300

    feat: novo exercicio de Estrutura de repetições

---
+++ Java/Estrutura de repetições/ex003/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        //imprima os primeiros 25 numeros de um dado valor. Por exemplo: 50
+        int valorMax = 50;
+        for (int i = 1; i <= valorMax; i++) {
+          if (i > 25) {
+            break;
+          }
+            System.out.println(i);
+        }
+    }
+}
