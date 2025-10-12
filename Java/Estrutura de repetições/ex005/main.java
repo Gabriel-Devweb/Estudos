a2fdbb2471d1eef0937c0e23d22edac5608a94fb
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-10-12 19:10:46 -0300

    Exercicio de continue

---
+++ Java/Estrutura de repetições/ex005/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        //Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
+        //Condição valorParcela >= 1000
+        double valorTotal = 30000;
+        for (int parcela = (int) valorTotal; parcela >= 1; parcela--) { 
+         double valorParcela = valorTotal / parcela;
+         if (valorParcela < 1000) {
+            continue;
+         }
+         System.out. println("Parcela "+ parcela+" R$ "+valorParcela);
+         }
+        }
+    }
