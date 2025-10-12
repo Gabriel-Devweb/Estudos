20dd36b80aabd523bd6facf5d2c90efcdea49607
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-10-12 20:43:01 -0300

    Aprendendo mais sobre a 'formatação' dos arrays

---
+++ Java/Arrays/ex002/main.java
@@ -0,0 +1,15 @@
+public class main {
+  public static void main(String[] args) {
+    //Byte, short,int float e double 0
+    //char '\u0000'
+    // boolean false
+    // String null
+    String[] nome = new String[3];
+    nome[0] = "Maria";
+    nome[1] = "Gabriel";
+    nome[2] = "Ayala";
+    for (int i = 0; i < 3; i++) {
+      System.out.println(nome[1]);
+    }
+  }
+}
