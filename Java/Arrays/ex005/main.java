81b7afa64615fc3103d097e060969ac946872a7c
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2025-10-14 10:02:24 -0300

    Aprendendo arrays multidimensionais

---
+++ Java/Arrays/ex005/main.java
@@ -0,0 +1,20 @@
+public class main {
+  public static void main(String[] args) {
+    // 1,2,3,4,5 Meses
+    // 32,28,31,30
+    int[][] dias = new int[3][3];
+    dias[0][0] = 31;
+    dias[0][1] = 28;
+    dias[0][2] = 31;
+
+    dias[1][0] = 31;
+    dias[1][1] = 28;
+    dias[1][2] = 31;
+    
+    for (int i = 0; i < dias.length; i++) {
+      for (int j = 0; j < dias[1].length; j++) {
+    System.out.println(dias[i][j]);
+      }
+    }
+  }
+}
